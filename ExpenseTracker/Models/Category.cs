ead943062d9442be56e88e7904a5d24ac3dc9c16
SDSD STAFF <SDSD STAFF@DESKTOP-3L41P6T>
2024-01-03 10:14:12 +0100

    Add project files.

---
+++ ExpenseTracker/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+    }
+}
